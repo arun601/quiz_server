a3a9b86764386f1e10c88acad06b8942a6b5b62a
Arun Yadav <arunyadav838481@gmail.com>
2023-09-06 06:47:54 -0700

    second

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{""}
+      <a
+        href="htt"
+        style={{ cursor: "pointer" }}
+      >
+        Arun yadav
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
